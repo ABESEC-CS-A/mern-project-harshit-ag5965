bcafc90da17dc28f19315a46cf619094086e55eb
harshit-ag5965 <agarwalharshit5965@gmail.com>
2025-11-13 11:46:21 +0530

    13-Nov-Work

---
+++ FSD_PROJECT/client/Lab_Experiment_Tracker/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='homep'>
+        <h1>Lab Experiment Tracker</h1>
+      
+    </div>
+  )
+}
+
+export default Header
